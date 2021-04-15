e4a7a5c08f0e03f7ea11f3188326e89fe2c597e8
Roman <roman.ziva@gmail.com>
2021-04-15 13:34:15 +0300

    int: [ch169342] fix filename for title

--- static/iframes-adapter.js
+++ static/iframes-adapter.js
@@ -1,47 +1,47 @@
 // jshint es3: false
 // jshint esversion: 6
 
-(function(karma) {
-	'use strict';
+(function (karma) {
+    'use strict';
 
-	var isDebug = document.location.href.indexOf('debug.html') > -1;
+    var isDebug = document.location.href.indexOf('debug.html') > -1;
 
-	function Suite(path, showTitle) {
+    function Suite(path, showTitle) {
         // state is one of
         // • 'pending' (before the total is known)
         // • 'started' (after total is known but before all suites have executed)
         // • 'complete' (when total === finished)
         this.state = 'pending';
-        this.fileName = path.match(/\/([^/]+)\.iframe\.html$/)[1];
+        this.fileName = path.match(/\/(absolute)?([^/]+)\.iframe\.html$/)[2].replace(/\d+\.js/, 'js');
         this.path = path;
         this.iframe = document.createElement('iframe');
         this.wrapper = document.createElement('span');
         this.showTitle = showTitle;
         this.total = NaN;
         this.finished = 0;
-	}
+    }
 
-	Suite.prototype.init = function(suites) {
-		if(isDebug) {
-			console.debug('Loaded suite ' + this.fileName);
-		}
-		// Add the suite as pending
+    Suite.prototype.init = function (suites) {
+        if (isDebug) {
+            console.debug('Loaded suite ' + this.fileName);
+        }
+        // Add the suite as pending
         var me = this;
-		suites[this.path] = this;
+        suites[this.path] = this;
 
-		var iframe = this.iframe;
+        var iframe = this.iframe;
 
-		// Remap console
+        // Remap console
         this.domContentLoadedListener = function () {
             iframe.contentWindow.console = console;
         };
         iframe.addEventListener('DOMContentLoaded', this.domContentLoadedListener, false);
 
-		// Listen to messages from the iFrame
-		this.messageListener = function (msg) {
-			if(!msg.source || iframe.contentWindow !== msg.source) {
-				return; // ignore messages from other iframes
-			}
+        // Listen to messages from the iFrame
+        this.messageListener = function (msg) {
+            if (!msg.source || iframe.contentWindow !== msg.source) {
+                return; // ignore messages from other iframes
+            }
 
             var data = msg.data;
 
@@ -53,30 +53,30 @@
                 }
             }
 
-			// Provide some namespace for the message
-			if(!Array.isArray(data) || data[0] !== 'iframe-test-results') {
-				return;
-			}
+            // Provide some namespace for the message
+            if (!Array.isArray(data) || data[0] !== 'iframe-test-results') {
+                return;
+            }
 
-			var message = data[1];
-			var arg = data[2];
+            var message = data[1];
+            var arg = data[2];
 
-			if(message === 'started') {
+            if (message === 'started') {
                 me.started(arg);
-			} else if(message === 'result') {
+            } else if (message === 'result') {
                 me.result(arg);
-			} else if(message === 'complete') {
+            } else if (message === 'complete') {
                 me.complete(arg);
-			} else {
-				// Other message (log, error); send directly to karma
-				karma[message].apply(karma, data.slice(2));
-			}
-		};
-		window.addEventListener('message', this.messageListener, false);
-	};
+            } else {
+                // Other message (log, error); send directly to karma
+                karma[message].apply(karma, data.slice(2));
+            }
+        };
+        window.addEventListener('message', this.messageListener, false);
+    };
 
-	Suite.prototype.run = function() {
-        if(isDebug) {
+    Suite.prototype.run = function () {
+        if (isDebug) {
             console.debug('Running suite ' + this.fileName);
         }
         if (this.showTitle) {
@@ -86,41 +86,41 @@
         this.wrapper.appendChild(this.iframe);
         this.iframe.src = this.path;
         document.body.appendChild(this.wrapper)
-	};
+    };
 
-	Suite.prototype.started = function(total) {
-		if(isDebug) {
+    Suite.prototype.started = function (total) {
+        if (isDebug) {
             console.debug('Suite ' + this.fileName + ' has started, expects ' + total + ' tests');
-		}
-		this.state = 'started';
-		this.total = total;
-		suiteStarted();
-	};
+        }
+        this.state = 'started';
+        this.total = total;
+        suiteStarted();
+    };
 
-	Suite.prototype.result = function(result) {
-		if(isDebug) {
+    Suite.prototype.result = function (result) {
+        if (isDebug) {
             console.debug('Suite ' + this.fileName + ' has a result, ' + result);
-		}
-		result.suite = result.suite || [];
-		result.suite.unshift(this.fileName.replace(/\.iframe\.html$/, ''));
-		result.id = this.fileName+'#'+(result.id || '');
-		this.finished++;
-		sendResult(result);
-	};
+        }
+        result.suite = result.suite || [];
+        result.suite.unshift(this.fileName.replace(/\.iframe\.html$/, ''));
+        result.id = this.fileName + '#' + (result.id || '');
+        this.finished++;
+        sendResult(result);
+    };
 
-	Suite.prototype.complete = function(result) {
-		if(isDebug) {
+    Suite.prototype.complete = function (result) {
+        if (isDebug) {
             console.debug('Suite ' + this.fileName + ' has completed with ' + this.finished + ' of ' + this.total + ' tests');
-		}
-		this.state = 'complete';
-		suiteComplete(result);
+        }
+        this.state = 'complete';
+        suiteComplete(result);
         this.onComplete();
-		this.cleanup();
-	};
+        this.cleanup();
+    };
 
-    Suite.prototype.onComplete = function() {};
+    Suite.prototype.onComplete = function () { };
 
-	Suite.prototype.cleanup = function() {
+    Suite.prototype.cleanup = function () {
         this.iframe.removeEventListener('DOMContentLoaded', this.domContentLoadedListener, false);
         this.iframe.parentNode.removeChild(this.iframe);
         this.wrapper.parentNode.removeChild(this.wrapper);
@@ -129,101 +129,101 @@
         this.wrapper = null;
         this.messageListener = null;
         this.domContentLoadedListener = null;
-	};
+    };
 
-	// Map suite files to suite instances
-	var suites = {};
+    // Map suite files to suite instances
+    var suites = {};
 
-	function suitesWithState(state) {
-		var isNeg = state[0] === '!';
-		if(isNeg) {
-			state = state.substr(1);
-		}
+    function suitesWithState(state) {
+        var isNeg = state[0] === '!';
+        if (isNeg) {
+            state = state.substr(1);
+        }
         var result = {};
-		Object.keys(suites)
-			.filter(function (path) {
-				return isNeg ? suites[path].state !== state : suites[path].state === state;
-			})
-			.forEach(function (path) {
-				result[path] = suites[path];
-			});
-		return result;
-	}
+        Object.keys(suites)
+            .filter(function (path) {
+                return isNeg ? suites[path].state !== state : suites[path].state === state;
+            })
+            .forEach(function (path) {
+                result[path] = suites[path];
+            });
+        return result;
+    }
 
-	function countTests() {
-		return Object.keys(suites)
-			.map(function (path) {return suites[path]; })
-			.reduce(function (sum, suite) {
+    function countTests() {
+        return Object.keys(suites)
+            .map(function (path) { return suites[path]; })
+            .reduce(function (sum, suite) {
                 var total = sum[0] + suite.total;
                 var finished = sum[1] + suite.finished;
-				return [total, finished];
-			}, [0, 0]);
-	}
+                return [total, finished];
+            }, [0, 0]);
+    }
 
-	function hasPendingSuites() {
+    function hasPendingSuites() {
         var startedSuites = suitesWithState('!pending');
-		return Object.keys(startedSuites).length < Object.keys(suites).length;
-	}
+        return Object.keys(startedSuites).length < Object.keys(suites).length;
+    }
 
-	var pendingResults = [];
-	function sendResult(result) {
-		if(hasPendingSuites()) {
-			// We should not send results to karma before all suites have started, queue them
-			pendingResults.push(result);
-			return;
-		}
-		// Send result directly
-		karma.result(result);
-	}
+    var pendingResults = [];
+    function sendResult(result) {
+        if (hasPendingSuites()) {
+            // We should not send results to karma before all suites have started, queue them
+            pendingResults.push(result);
+            return;
+        }
+        // Send result directly
+        karma.result(result);
+    }
 
-	// Some suite has started
-	function suiteStarted() {
-		// Have all suites started?
-		if(hasPendingSuites()) {
-			return;
-		}
-		// All suites have started, send the total to karma
+    // Some suite has started
+    function suiteStarted() {
+        // Have all suites started?
+        if (hasPendingSuites()) {
+            return;
+        }
+        // All suites have started, send the total to karma
         var cntT = countTests();
         var total = cntT[0],
-            finished =cntT[1];
+            finished = cntT[1];
 
-		if(isDebug) {
+        if (isDebug) {
             console.debug('All ' + Object.keys(suites).length + ' suites have started, expecting ' + total + ' tests (of which ' + finished + ' have already finished)');
-		}
-		karma.info({total: total});
-		// Send the pending results
-		pendingResults.forEach(sendResult);
-		pendingResults = [];
-	}
+        }
+        karma.info({ total: total });
+        // Send the pending results
+        pendingResults.forEach(sendResult);
+        pendingResults = [];
+    }
 
-	// Some suite has completed
-	function suiteComplete(result) {
+    // Some suite has completed
+    function suiteComplete(result) {
         if (result.coverage) {
             coverageCollector.addCoverage(result.coverage);
         }
 
-		// Have all suites completed?
+        // Have all suites completed?
         var completedSuites = suitesWithState('complete');
-		if(Object.keys(completedSuites).length < Object.keys(suites).length) {
+        if (Object.keys(completedSuites).length < Object.keys(suites).length) {
             result.coverage = null;
-			return;
-		}
-		// All suites have completed, send the “complete” message to karma
-		if(isDebug) {
+            return;
+        }
+        // All suites have completed, send the “complete” message to karma
+        if (isDebug) {
             var cntT = countTests();
             var total = cntT[0],
-                finished =cntT[1];
+                finished = cntT[1];
             console.debug('All ' + Object.keys(suites).length + ' suites have completed, ran ' + finished + ' of ' + total + ' tests');
-		}
+        }
         if (result.coverage) {
             result.coverage = coverageCollector.getCoverage();
         }
-		karma.complete(result);
-	}
+        karma.complete(result);
+    }
 
-    function start () {
+    function start() {
         // jshint validthis: true
-        var files = Object.keys(karma.files).filter(function (file) {return file.match(/\.iframe\.html$/);});
+        var files = Object.keys(karma.files).filter(function (file) { return file.match(/\.iframe\.html$/); });
         var concurrency = parseInt(karma.config.concurrency, 10) || 10;
         var showFrameTitle = karma.config.showFrameTitle || false;
         var ran = 0;
@@ -236,7 +236,7 @@
 
         preparedSuites.reverse();
 
-        function runNextSuite () {
+        function runNextSuite() {
             var suite = preparedSuites.pop();
             if (!suite) {
                 return;
@@ -322,6 +322,6 @@
         }
     };
 
-	karma.start = start;
+    karma.start = start;
 })(window.__karma__);
 
